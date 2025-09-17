8c318a19686bc8e36c2b878a75fdcc8b2781e8b9
Taisuke Oe <oeuia.t@gmail.com>
2025-09-17 15:06:52 +0900

    Simplify bug-11014 comparison fixture

--- tests/PHPStan/Rules/Comparison/data/bug-11014.php
+++ tests/PHPStan/Rules/Comparison/data/bug-11014.php
@@ -7,14 +7,10 @@ class HelloWorld
     /** @param string[] $values */
     public function sayHello(array $values): void
     {
-        array_map(function ($item): string {
-            if (!is_string($item)) {
+        foreach ($values as $value) {
+            if (!is_string($value)) {
                 throw new \Exception();
             }
-
-            return $item;
-        }, $values);
-
-        array_map(fn ($item): string => is_string($item) ? $item : throw new \Exception(), $values);
+        }
     }
 }
